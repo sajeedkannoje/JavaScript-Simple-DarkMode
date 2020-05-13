add5a2699b02674e8822d08463350112beca75cf
Sajeed Kannoje <63125230+sajeedkhan18@users.noreply.github.com>
2020-05-13 12:18:54 +0530

    Add files via upload
    dark mode using javascript

---
+++ js.js
@@ -0,0 +1,11 @@
+
+
+
+document.getElementById("darkmode").addEventListener("click",dark);
+
+
+function dark(){
+document.getElementById("darkmode").innerText="LightMode";
+document.getElementById("home").classList.toggle("dark");
+
+}
